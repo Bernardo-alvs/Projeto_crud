620e6bbfb6df395c9a89a46492a31e347c1597a1
CCapparelli <kdcapparelli@gmail.com>
2024-10-03 10:09:30 -0300

    Atv_0310

---
+++ conexao.py
@@ -0,0 +1,14 @@
+import mysql
+import mysql.connector
+
+conn = mysql.connector.connect(
+    username = ' Bernardo' ,
+    host = 'localhost' ,
+    password ='projeto123',
+    db = 'projeto_crud'
+
+)
+if conn.is_connected():
+    print('Conectado com sucesso!!!!')
+else:
+    print('Falha ao conectar')
