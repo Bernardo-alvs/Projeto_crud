1f8f8c9f4439b829bf8e94ad307d42b76eaea1b0
CCapparelli <kdcapparelli@gmail.com>
2024-10-03 10:32:03 -0300

    ATV0310

--- conexao.py
+++ conexao.py
@@ -4,11 +4,11 @@ import mysql.connector
 conn = mysql.connector.connect(
     username = ' Bernardo' ,
     host = 'localhost' ,
-    password ='projeto123',
+    password = 'projeto123',
     db = 'projeto_crud'
 
 )
 if conn.is_connected():
     print('Conectado com sucesso!!!!')
 else:
-    print('Falha ao conectar')
+    print('Falha na conexão')
